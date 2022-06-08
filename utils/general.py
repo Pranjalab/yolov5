46ef50deb5a45515f82e2fdaa95ba7b5164fb659
Pranjal Bhaskare <pranjalab@gmail.com>
2022-06-08 17:27:00 +0530

    fix: commited yolo submodule

--- utils/general.py
+++ utils/general.py
@@ -33,8 +33,8 @@ import torch
 import torchvision
 import yaml
 
-from utils.downloads import gsutil_getsize
-from utils.metrics import box_iou, fitness
+from submodules.yolov5.utils.downloads import gsutil_getsize
+from submodules.yolov5.utils.metrics import box_iou, fitness
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[1]  # YOLOv5 root directory
